1ee9b25770db0f60c3d8105bc2aa3640e3c6718d
yoelvp <yoelvp73@gmail.com>
2023-11-17 22:04:24 -0500

    reafctor code and structure of programming

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
 use prettytable::{color, Attr, Cell, Row, Table};
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
-use std::error::Error;
-use std::fs;
-use std::io::{self, Write, Result};
+use std::{
+    io::{self, Write},
+    process,
+};
 
 #[derive(Debug, Clone)]
 struct TodoItem {
-    id: String,
+    id: u32,
     title: String,
     description: String,
     done: bool,
 }
 
-// ?: Generate random UUID
-fn generate_uuid() -> String {
-    let lenght = 24;
-    thread_rng()
-        .sample_iter(Alphanumeric)
-        .take(lenght)
-        .map(char::from)
-        .collect()
+fn generate_id(todos: &Vec<TodoItem>) -> u32 {
+    (todos.len() + 1).try_into().unwrap()
 }
 
 fn paint_cell(text: String, color: String) {
@@ -31,60 +24,30 @@ fn paint_cell(text: String, color: String) {
     println!("+{}{}+\x1b[0m", color, dashes);
 }
 
-// Formats the obtained text to the TodoItem struct
-fn parse_todo_item(todo_string: &str) -> Option<TodoItem> {
-    let lines: Vec<&str> = todo_string.lines().collect();
-
-    if lines.len() == 4 {
-        let id = lines[0].to_string();
-        let title = lines[1].to_string();
-        let description = lines[2].to_string();
-        let done = lines[3].to_string();
-
-        Some(TodoItem { id, title, description, done })
-    } else {
-        None
-    }
-}
-
 // Save a new TODO to file
-fn write_new_todo() {}
-
-// Get TODOS from file
-fn read_all_todos(file_path: &str, todos: &mut Vec<TodoItem>) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(file_path)?;
-    let todos_from_file: Vec<&str> = content.split("\n\n").collect();
-
-    for todo in todos_from_file {
-        if let Some(todo_item) = parse_todo_item(todo) {
-            todos.push(todo_item)
-        }
-    }
-
-    Ok(())
+fn write_new_todo() {
+    // TODO: write a new todo in file
 }
 
 // Function [main]
 fn main() {
-    let file_path = "TODOS";
     let mut todos: Vec<TodoItem> = Vec::new();
-    let mut todos = read_all_todos(file_path, &mut todos);
+    // let mut todos = read_all_todos(file_path, &mut todos);
 
     let todo_1 = TodoItem {
-        id: generate_uuid(),
+        id: generate_id(&todos),
         title: String::from("Todo 01"),
         description: String::from("Description of todo 01"),
         done: false,
     };
+    todos.push(todo_1);
     let todo_2 = TodoItem {
-        id: generate_uuid(),
+        id: generate_id(&todos),
         title: String::from("Todo 02"),
         description: String::from("Description of todo 02"),
         done: false,
     };
-
-    // todos.push(todo_1);
-    // todos.push(todo_2);
+    todos.push(todo_2);
 
     loop {
         let mut input = String::new();
@@ -104,7 +67,7 @@ fn main() {
                 .with_style(Attr::ForegroundColor(color::GREEN)),
         ]));
 
-        println!("What are you going to do?: \x1b[32m[a]\x1b[0m Add  \x1b[32m[l]\x1b[0m To list  \x1b[32m[d]\x1b[0m Delete  \x1b[32m[e]\x1b[0m Edit  \x1b[32m[q]\x1b[0m Quit");
+        println!("What are you going to do?: \x1b[32m[a]\x1b[0mdd  \x1b[32m[l]\x1b[0mist  \x1b[32m[d]\x1b[0melete  \x1b[32m[e]\x1b[0mdit  \x1b[32m[q]\x1b[0muit");
         print!("> ");
         io::stdout().flush().unwrap();
         io::stdin()
@@ -132,7 +95,7 @@ fn main() {
                 println!();
 
                 let new_todo = TodoItem {
-                    id: generate_uuid(),
+                    id: generate_id(&todos),
                     title,
                     description,
                     done: false,
@@ -142,7 +105,7 @@ fn main() {
 
                 for todo in todos.iter() {
                     table.add_row(Row::new(vec![
-                        Cell::new(&todo.id),
+                        Cell::new(&todo.id.to_string()),
                         Cell::new(&todo.title),
                         Cell::new(&todo.description),
                         Cell::new(&todo.done.to_string()),
@@ -155,18 +118,18 @@ fn main() {
             "l" => {
                 for todo in todos.iter() {
                     table.add_row(Row::new(vec![
-                        Cell::new(&todo.id),
+                        Cell::new(&todo.id.to_string()),
                         Cell::new(&todo.title),
                         Cell::new(&todo.description),
                         Cell::new(&todo.done.to_string()),
                     ]));
                 }
                 table.printstd();
-                println!();
+                println!()
             }
             "d" => loop {
                 let mut input = String::new();
-                print!("Enter th first 3 characters of the TODO ID to delete > ");
+                print!("Enter the ID of the TODO ID to delete it > ");
                 io::stdout().flush().unwrap();
                 io::stdin()
                     .read_line(&mut input)
@@ -174,10 +137,12 @@ fn main() {
 
                 let todo_id = input.trim();
 
-                if todo_id.len() <= 3 && todo_id.len() > 2 {
+                if todos.len() > 0 {
                     let found_todo = todos.iter().find(|&todo| {
-                        let first_tree_chars_of_id: String = todo.id.chars().take(3).collect();
-                        first_tree_chars_of_id == todo_id
+                        let id_entered_by_user = todo_id
+                            .parse()
+                            .expect("There was an error converting the ID entered by the user");
+                        todo.id == id_entered_by_user
                     });
 
                     match found_todo {
@@ -192,7 +157,7 @@ fn main() {
                                 println!("\x1b[32m");
                                 println!("The TODO has been successfully deleted\n");
                                 table_removed_todo.add_row(Row::new(vec![
-                                    Cell::new(&removed_todo.id),
+                                    Cell::new(&removed_todo.id.to_string()),
                                     Cell::new(&removed_todo.title),
                                     Cell::new(&removed_todo.description),
                                     Cell::new(&removed_todo.done.to_string()),
@@ -209,14 +174,9 @@ fn main() {
                         None => paint_cell(String::from("TODO does not exist"), String::from("")),
                     }
                     break;
-                } else if todos.len() < 1 {
-                    paint_cell(
-                        String::from("No TODOS available, add a TODO first"),
-                        String::from("\x1b[33m"),
-                    );
                 } else {
                     paint_cell(
-                        String::from("Enter only 3 characters"),
+                        String::from("No TODOS available, add a TODO first"),
                         String::from("\x1b[33m"),
                     );
                 }
@@ -231,10 +191,12 @@ fn main() {
 
                 let todo_id = input.trim();
 
-                if todo_id.len() <= 3 && todo_id.len() > 2 {
+                if todos.len() > 0 {
                     let found_todo = todos.iter().find(|&todo| {
-                        let first_tree_chars_of_id: String = todo.id.chars().take(3).collect();
-                        first_tree_chars_of_id == todo_id
+                        let id_entered_by_user = todo_id
+                            .parse()
+                            .expect("There was an error converting the ID entered by the user");
+                        todo.id == id_entered_by_user
                     });
 
                     match found_todo {
@@ -264,7 +226,7 @@ fn main() {
                                 let mut table_edited_todo = Table::new();
                                 println!("The TODO has been successfully edited\n");
                                 table_edited_todo.add_row(Row::new(vec![
-                                    Cell::new(&todos[index].id),
+                                    Cell::new(&todos[index].id.to_string()),
                                     Cell::new(&todos[index].title),
                                     Cell::new(&todos[index].description),
                                     Cell::new(&todos[index].done.to_string()),
@@ -280,26 +242,24 @@ fn main() {
                         None => paint_cell(String::from("TODO does not exist"), String::from("")),
                     }
                     break;
-                } else if todos.len() < 1 {
+                } else {
                     paint_cell(
                         String::from("No TODOS available, add a TODO first"),
                         String::from("\x1b[33m"),
                     );
-                } else {
-                    paint_cell(
-                        String::from("Enter only 3 characters"),
-                        String::from("\x1b[33m"),
-                    );
                 }
             },
-            "q" => break,
+            "q" => {
+                // break;
+                process::exit(0);
+            }
             _ => {
                 let mut table_invalid_option = Table::new();
                 table_invalid_option
                     .add_row(Row::new(vec![Cell::new("Not a valid option")
                         .with_style(Attr::ForegroundColor(color::RED))]));
                 table_invalid_option.printstd();
-                println!();
+                println!()
             }
         }
     }
